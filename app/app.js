c829aa80e0b8ee4ddbc2ff8e2549e74197dca28e
Julio Disla <20111614@ce.pucmm.edu.do>
2019-06-28 19:04:55 +0000

    firtst

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
-/*eslint-env node*/
-
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hola Mundo!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/hola', function (req, res) {
+  res.send('Hola Mundo!');
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 });
