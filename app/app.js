89af4a79be09163f1946d8c48286827cfb840a14
Julio Disla <20111614@ce.pucmm.edu.do>
2019-06-28 21:27:25 +0000

    weired

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-express.
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,16 @@ gulp.task('style',function(){
     return gulp.src(Files)
     .pipe(hint());
 });
+
+gulp.task('inject',function(){
+   
+   var wire = require('wiredep').stream;
+   var options ={
+       bowerJson: require('./bower.json'),
+       directory: './bower_components'
+       }
+   return gulp.src('.src/views/*.html')
+   .pipe(wire(options))
+   .pipe(gulp.dest('./src/views'));
+   ;
+});
