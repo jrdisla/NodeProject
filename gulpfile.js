2a9921f726156ba8884edb08bef53a395da2cd1d
Julio Disla <20111614@ce.pucmm.edu.do>
2019-06-29 17:00:04 +0000

    inject

--- gulpfile.js
+++ gulpfile.js
@@ -24,13 +24,21 @@ gulp.task('inject',function(){
 
 
 
-var wiredep = require('wiredep').stream;
- 
+
+
 gulp.task('bower', function () {
- return gulp.src('./src/views/*.html')
+var wiredep = require('wiredep').stream;
+var inject =  require('gulp-inject'); 
+var injectSrc = gulp.src(['./public/css/*.css','./public/js/*.js']);
+var injectOptions = {
+    ignorePath: '/public/'
+}
+ return gulp.src('./src/views/index.html')
     .pipe(wiredep({
        bowerJson: require('./bower.json'),
-       directory: './bower_components'
+       directory: './bower_components',
+       ignorePath: '../../bower_components'
     }))
+    .pipe(inject(injectSrc,injectOptions))
     .pipe(gulp.dest('./src/views'));
 });
